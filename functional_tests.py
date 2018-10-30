7c9d942337957862dae20856bf74d9c70720ee39
Ryan Bahr <Ry.bahr@gmail.com>
2018-10-30 15:02:32 -0700

    Function test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,8 +8,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    #def tearDown(self):
-    #    self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
     def test_can_start_a_list_and_retreive_it_later(self):
         #Mike, as a 60-year-old man, has memory problems.
@@ -17,16 +18,29 @@ class NewVisitorTest(unittest.TestCase):
 
         #After seeing "To-Do" in the page header
         self.assertIn('To-Do', self.browser.title)
-        #self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         #He is invited to enter a to-do item straight away
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         #He Types "Buy Marine Sniper Rifle into a text-box.
+        input.send_keys('Buy Marine Sniper Rifle')
         #When he hits enter, the page updates, and now the page lists:
-    #"1: Buy Marine Sniper Rifle"
+        #"1: Buy Marine Sniper Rifle"
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy Marine Sniper Rifle' for row in rows)
+        )
     #There is still a text book which invites Mike to add another itemself.
     #He enters: "Kill Tuco Salamanca with the Sniper Rifle."
-
+        self.fail('Finish the test!')
     #The page updates again, now showing both items on the listself.
 
     #Concerned over losing his progress, Mike sees that the site has
