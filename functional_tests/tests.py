3c3b1e24481400243c6a709e9e3f1f12762b61fd
Ryan Bahr <Ry.bahr@gmail.com>
2018-11-09 16:50:11 -0800

    first steps of FT for layout + stylings

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,18 +15,6 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_layout_and_styling(self):
-        #Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        #she notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -126,12 +114,24 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+    def test_layout_and_styling(self):
+        #edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
-        #she starts a new list and sees the input is nicely
-        #centered in there as well
+        #she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        #she starts a new list, and sees the input is nicely
+        #centered there as well
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('2: testing')
+        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
